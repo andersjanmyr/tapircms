3974d3b7f0ff30e3bed35e30b87a6d9fef5bf748
Anders Janmyr <anders@janmyr.com>
2013-01-27 11:54:42 +0100

    Added partial spec

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -1,28 +1,40 @@
 require 'spec_helper'
 
 describe Page do
-  let(:page) { Fabricate.build(:page) }
 
-  it 'has a title' do
-    expect(page.title).to be
+  context 'default' do
+    let(:page) { Fabricate.build(:page) }
+
+    it 'has a title' do
+      expect(page.title).to be
+    end
+
+    it 'has a description' do
+      expect(page.description).to be
+    end
+
+    it 'has a slug' do
+      expect(page.slug).to be
+    end
+
+    it 'has one frame' do
+      page.frame = Fabricate.build(:frame)
+      expect(page.frame).to be
+    end
+
+    it 'has many entries' do
+      page.entries << Fabricate.build(:page_entry)
+      page.entries << Fabricate.build(:page_entry)
+      expect(page.entries).to have(2).items
+    end
   end
 
-  it 'has a description' do
-    expect(page.description).to be
-  end
+ context 'partial' do
+   let(:frame) { Frame.new(template: '_default') }
 
-  it 'has a slug' do
-    expect(page.slug).to be
-  end
-
-  it 'has one frame' do
-    page.frame = Fabricate.build(:frame)
-    expect(page.frame).to be
-  end
-
-  it 'has many entries' do
-    page.entries << Fabricate.build(:page_entry)
-    page.entries << Fabricate.build(:page_entry)
-    expect(page.entries).to have(2).items
-  end
+   it 'returns the frame.template as a partial' do
+     page = Page.new(frame: frame)
+     expect(page.partial).to eq('/sites/frames/_default.html')
+   end
+ end
 end
