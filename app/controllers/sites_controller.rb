6b341b7eea12eb2d46fc549829bdd3e86cab9d06
Anders Janmyr <anders@janmyr.com>
2013-01-27 11:32:53 +0100

    Use helper_method instead of instance vars for view

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,14 @@
 class SitesController < ApplicationController
   def index
+  end
+
+  private
+  helper_method :site, :page
+  def site
+    @site = Site.new
+  end
+
+  def page
     @page = Page.new(frame: Frame.new(template: 'default'))
   end
 end
