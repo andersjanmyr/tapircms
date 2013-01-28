e7abba72d55388eaf7e1b68a0810d51e15bd30d2
Anders Janmyr <anders@janmyr.com>
2013-01-28 09:13:39 +0100

    Fixed article images

--- app/models/feeds/tapir_feed.rb
+++ app/models/feeds/tapir_feed.rb
@@ -1,6 +1,6 @@
 class Feeds::TapirFeed < Feed
 
   def articles
-    (1..10).map { |i| Article.new(title: 'Tapirs Rule!', abstract: 'The world!', images: %(/images/tapir.png)) }
+    (1..10).map { |i| Article.new(title: 'Tapirs Rule!', abstract: 'The world!', images: %w(tapir.png)) }
   end
 end
